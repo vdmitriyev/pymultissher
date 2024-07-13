49767393f60e0ae961a54eeca1e9334f6691bc14
Dmitriyev <2291074+vdmitriyev@users.noreply.github.com>
2024-07-13 18:08:46 +0200

    ADD: taskfile version to be used instead of make

---
+++ scripts/cmdInitiateEnv.bat
@@ -0,0 +1,18 @@
+@echo off
+cd ..
+setlocal
+:PROMPT
+SET AREYOUSURE=N
+SET /P AREYOUSURE=Do you want to create new virtual environment (Y/[N])? 
+IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
+
+SET PATH=C:\Compilers\Python312\Scripts\;C:\Compilers\Python312\;%PATH%
+python -m venv .venv
+call .\.venv\Scripts\activate.bat
+python -m pip install --upgrade pip
+pip install uv
+uv pip install -r requirements-prod.txt
+uv pip install -r requirements-dev.txt
+
+:END
+endlocal
---
+++ scripts/cmdStartEnv.bat
@@ -0,0 +1,5 @@
+@echo off
+cd ..
+SET PATH=C:\Compilers\Python312\Scripts\;C:\Compilers\Python312\;%PATH%
+call .\.venv\Scripts\activate.bat
+cmd
