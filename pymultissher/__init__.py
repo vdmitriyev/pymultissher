8c904529847e6f277668b3c6ea923a987ac920bd
Dmitriyev <2291074+vdmitriyev@users.noreply.github.com>
2024-10-11 16:54:02 +0200

    EDIT: update pre-commit packages

--- pymultissher/__init__.py
+++ pymultissher/__init__.py
@@ -1,4 +1,4 @@
-"""Top-level package for pymultissher."""
+"""Top-level package"""
 
 __license__ = "MIT"
 __description__ = "pymultissher is a simple CLI tool that runs commands on multiple servers over SSH"
